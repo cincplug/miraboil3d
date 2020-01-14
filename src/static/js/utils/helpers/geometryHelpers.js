5b7389149eb4bbdabf6977504c5872173f878af7
Luka Stanisavljevic <lstanisavljevic@mirabeau.nl>
2020-01-14 13:34:40 +0100

    develop: add more helpers

--- src/static/js/utils/helpers/geometryHelpers.js
+++ src/static/js/utils/helpers/geometryHelpers.js
@@ -17,11 +17,54 @@ const geometryHelpers = {
   },
 
   parametric: {
+    plane(geometry) {
+      return function(u, v, target) {
+        const x = u * geometry.width
+        const y = v * geometry.height
+        const z = 0
+        target.set(x, y, z)
+      }
+    },
+    basket(geometry) {
+      const { emboss, range } = geometry
+      return function(u, v, target) {
+        const x = u * geometry.width
+        const y = v * geometry.height
+        let z = 0
+        if (
+          u > range.startX &&
+          u < range.endX &&
+          v > range.startY &&
+          v < range.endY
+        ) {
+          z += emboss
+        }
+        target.set(x, y, z)
+      }
+    },
     bentPicture(geometry) {
       return function(u, v, target) {
         const x = u * geometry.width
         const y = v * geometry.height
         const z = Math.sin(v + u) * geometry.curvature
+        target.set(x, y, z)
+      }
+    },
+    folderBox(geometry) {
+      const { emboss, range } = geometry
+      return function(u, v, target) {
+        const x = u * geometry.width
+        let y = v * geometry.height
+        let z = Math.sin(v * geometry.emboss) * geometry.curvature
+        if (
+          u > range.startX &&
+          u < range.endX &&
+          v > range.startY &&
+          v < range.endY
+        ) {
+          y += emboss
+          z -= emboss
+        }
         target.set(x, y, z)
       }
     }
